e04fb53a586d1aec732219a46b4b05cbc8fcab59
Hocuri <hocuri@gmx.de>
2020-06-07 20:45:55 +0200

    rustfmt

--- src/address.rs
+++ src/address.rs
@@ -1,10 +1,9 @@
 use std::fmt;
 use std::str::FromStr;
 
-use super::rfc5322::{Rfc5322Parser, MIME_LINE_LENGTH};
 use super::header::{FromHeader, ToFoldedHeader};
-use super::results::{ParsingResult,ParsingError};
-
+use super::results::{ParsingError, ParsingResult};
+use super::rfc5322::{Rfc5322Parser, MIME_LINE_LENGTH};
 
 /// Represents an RFC 5322 Address
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -68,7 +67,7 @@ impl Mailbox {
     pub fn new(address: String) -> Mailbox {
         Mailbox {
             name: None,
-            address: address
+            address: address,
         }
     }
 
@@ -76,7 +75,7 @@ impl Mailbox {
     pub fn new_with_name(name: String, address: String) -> Mailbox {
         Mailbox {
             name: Some(name),
-            address: address
+            address: address,
         }
     }
 }
@@ -85,7 +84,8 @@ impl fmt::Display for Mailbox {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match self.name {
             Some(ref name) => {
-                let s = encoded_words::encode(name, None, encoded_words::EncodingFlag::Shortest, None);
+                let s =
+                    encoded_words::encode(name, None, encoded_words::EncodingFlag::Shortest, None);
                 write!(fmt, "{} <{}>", s, self.address)
             }
             None => write!(fmt, "<{}>", self.address),
@@ -163,7 +163,7 @@ pub struct AddressParser<'s> {
 impl<'s> AddressParser<'s> {
     pub fn new(s: &str) -> AddressParser {
         AddressParser {
-            p: Rfc5322Parser::new(s)
+            p: Rfc5322Parser::new(s),
         }
     }
 
@@ -179,16 +179,19 @@ impl<'s> AddressParser<'s> {
                     // Is a group
                     result.push(x);
                     expected_separator = ';';
-                },
+                }
                 Err(e) => {
                     // If we failed to parse as group, try again as mailbox
                     self.p.pop_position();
                     result.push(Address::Mailbox(match self.parse_mailbox() {
                         Ok(x) => x,
-                        Err(e2) => return Err(ParsingError::new(
-                            format!("Failed to parse as group: {}\n\
-                                     Failed to parse as mailbox: {}", e, e2)
-                        ))
+                        Err(e2) => {
+                            return Err(ParsingError::new(format!(
+                                "Failed to parse as group: {}\n\
+                                     Failed to parse as mailbox: {}",
+                                e, e2
+                            )))
+                        }
                     }));
                     expected_separator = ',';
                 }
@@ -207,7 +210,12 @@ impl<'s> AddressParser<'s> {
     pub fn parse_group(&mut self) -> ParsingResult<Address> {
         let name = match self.p.consume_phrase(false) {
             Some(x) => x,
-            None => return Err(ParsingError::new(format!("Couldn't find group name: {}", self.p.peek_to_end())))
+            None => {
+                return Err(ParsingError::new(format!(
+                    "Couldn't find group name: {}",
+                    self.p.peek_to_end()
+                )))
+            }
         };
 
         try!(self.p.assert_char(':'));
@@ -221,7 +229,7 @@ impl<'s> AddressParser<'s> {
             if !self.p.eof() && self.p.peek() == ',' {
                 self.p.consume_char();
             }
-        };
+        }
 
         Ok(Address::Group(name, mailboxes))
     }
@@ -250,11 +258,13 @@ impl<'s> AddressParser<'s> {
         let addr = try!(self.parse_addr_spec());
         if self.p.consume_char() != Some('>') {
             // Fail because we should have a closing RANGLE here (to match the opening one)
-            Err(ParsingError::new("Missing '>' at end while parsing address header.".to_string()))
+            Err(ParsingError::new(
+                "Missing '>' at end while parsing address header.".to_string(),
+            ))
         } else {
             Ok(match display_name {
                 Some(name) => Mailbox::new_with_name(name, addr),
-                None => Mailbox::new(addr)
+                None => Mailbox::new(addr),
             })
         }
     }
@@ -263,7 +273,11 @@ impl<'s> AddressParser<'s> {
         // local-part is a phrase, but allows dots in atoms
         let local_part = match self.p.consume_phrase(true) {
             Some(x) => x,
-            None => return Err(ParsingError::new(format!("Couldn't find local part while parsing address.")))
+            None => {
+                return Err(ParsingError::new(format!(
+                    "Couldn't find local part while parsing address."
+                )))
+            }
         };
 
         try!(self.p.assert_char('@'));
@@ -277,7 +291,7 @@ impl<'s> AddressParser<'s> {
         // TODO: support domain-literal
         match self.p.consume_atom(true) {
             Some(x) => Ok(x),
-            None => Err(ParsingError::new("Failed to parse domain.".to_string()))
+            None => Err(ParsingError::new("Failed to parse domain.".to_string())),
         }
     }
 }
@@ -293,13 +307,19 @@ mod tests {
         let addr = Mailbox::new("foo@example.org".to_string());
         assert_eq!(addr.to_string(), "<foo@example.org>".to_string());
 
-        let name_addr = Mailbox::new_with_name("Joe Blogs".to_string(), "foo@example.org".to_string());
-        assert_eq!(name_addr.to_string(), "=?utf-8?q?Joe_Blogs?= <foo@example.org>");
+        let name_addr =
+            Mailbox::new_with_name("Joe Blogs".to_string(), "foo@example.org".to_string());
+        assert_eq!(
+            name_addr.to_string(),
+            "=?utf-8?q?Joe_Blogs?= <foo@example.org>"
+        );
     }
 
     #[test]
     fn test_address_from_string() {
-        let addr = "\"Joe Blogs\" <joe@example.org>".parse::<Mailbox>().unwrap();
+        let addr = "\"Joe Blogs\" <joe@example.org>"
+            .parse::<Mailbox>()
+            .unwrap();
         assert_eq!(addr.name.unwrap(), "Joe Blogs".to_string());
         assert_eq!(addr.address, "joe@example.org".to_string());
 
@@ -321,8 +341,10 @@ mod tests {
 
     #[test]
     fn test_invalid_address_parsing() {
-	for a in vec!["<a@example.com", "a@"].iter() {
-            AddressParser::new(a).parse_mailbox().expect_err("parse failure");
+        for a in vec!["<a@example.com", "a@"].iter() {
+            AddressParser::new(a)
+                .parse_mailbox()
+                .expect_err("parse failure");
         }
     }
 
@@ -331,61 +353,106 @@ mod tests {
         let addr = Address::new_group("undisclosed recipients".to_string(), vec![]);
         assert_eq!(addr.to_string(), "undisclosed recipients: ;".to_string());
 
-        let addr = Address::new_group("group test".to_string(), vec![
-            Mailbox::new("joe@example.org".to_string()),
-            Mailbox::new_with_name("John Doe".to_string(), "john@example.org".to_string()),
-        ]);
-        assert_eq!(addr.to_string(), "group test: <joe@example.org>, =?utf-8?q?John_Doe?= <john@example.org>;");
+        let addr = Address::new_group(
+            "group test".to_string(),
+            vec![
+                Mailbox::new("joe@example.org".to_string()),
+                Mailbox::new_with_name("John Doe".to_string(), "john@example.org".to_string()),
+            ],
+        );
+        assert_eq!(
+            addr.to_string(),
+            "group test: <joe@example.org>, =?utf-8?q?John_Doe?= <john@example.org>;"
+        );
     }
 
     #[test]
     fn test_address_group_parsing() {
-        let mut parser = AddressParser::new("A Group:\"Joe Blogs\" <joe@example.org>,john@example.org;");
+        let mut parser =
+            AddressParser::new("A Group:\"Joe Blogs\" <joe@example.org>,john@example.org;");
         let addr = parser.parse_group().unwrap();
         match addr {
             Address::Group(name, mboxes) => {
                 assert_eq!(name, "A Group".to_string());
-                assert_eq!(mboxes, vec![
-                    Mailbox::new_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-                    Mailbox::new("john@example.org".to_string()),
-                ]);
-            },
+                assert_eq!(
+                    mboxes,
+                    vec![
+                        Mailbox::new_with_name(
+                            "Joe Blogs".to_string(),
+                            "joe@example.org".to_string()
+                        ),
+                        Mailbox::new("john@example.org".to_string()),
+                    ]
+                );
+            }
             _ => assert!(false),
         }
     }
 
     #[test]
     fn test_address_list_parsing() {
-        let mut parser = AddressParser::new("\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>");
-        assert_eq!(parser.parse_address_list().unwrap(), vec![
-            Address::new_mailbox_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-            Address::new_mailbox_with_name("John Doe".to_string(), "john@example.org".to_string()),
-        ]);
+        let mut parser =
+            AddressParser::new("\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>");
+        assert_eq!(
+            parser.parse_address_list().unwrap(),
+            vec![
+                Address::new_mailbox_with_name(
+                    "Joe Blogs".to_string(),
+                    "joe@example.org".to_string()
+                ),
+                Address::new_mailbox_with_name(
+                    "John Doe".to_string(),
+                    "john@example.org".to_string()
+                ),
+            ]
+        );
     }
 
     #[test]
     fn test_address_list_parsing_groups() {
         let mut parser = AddressParser::new("A Group:\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>; <third@example.org>, <fourth@example.org>");
-        assert_eq!(parser.parse_address_list().unwrap(), vec![
-            Address::new_group("A Group".to_string(), vec![
-                    Mailbox::new_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-                    Mailbox::new_with_name("John Doe".to_string(), "john@example.org".to_string()),
-            ]),
-            Address::new_mailbox("third@example.org".to_string()),
-            Address::new_mailbox("fourth@example.org".to_string()),
-        ]);
+        assert_eq!(
+            parser.parse_address_list().unwrap(),
+            vec![
+                Address::new_group(
+                    "A Group".to_string(),
+                    vec![
+                        Mailbox::new_with_name(
+                            "Joe Blogs".to_string(),
+                            "joe@example.org".to_string()
+                        ),
+                        Mailbox::new_with_name(
+                            "John Doe".to_string(),
+                            "john@example.org".to_string()
+                        ),
+                    ]
+                ),
+                Address::new_mailbox("third@example.org".to_string()),
+                Address::new_mailbox("fourth@example.org".to_string()),
+            ]
+        );
     }
 
     #[test]
     fn test_from_header_parsing() {
         let header = Header::new(
             "From:".to_string(),
-            "\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>".to_string());
+            "\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>".to_string(),
+        );
         let addresses: Vec<Address> = header.get_value().unwrap();
-        assert_eq!(addresses, vec![
-            Address::new_mailbox_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-            Address::new_mailbox_with_name("John Doe".to_string(), "john@example.org".to_string()),
-        ]);
+        assert_eq!(
+            addresses,
+            vec![
+                Address::new_mailbox_with_name(
+                    "Joe Blogs".to_string(),
+                    "joe@example.org".to_string()
+                ),
+                Address::new_mailbox_with_name(
+                    "John Doe".to_string(),
+                    "john@example.org".to_string()
+                ),
+            ]
+        );
     }
 
     #[test]
@@ -396,8 +463,10 @@ mod tests {
         ];
 
         let header = Header::new_with_value("From:".to_string(), addresses).unwrap();
-        assert_eq!(header.get_value::<String>().unwrap(),
-                   "Joe_Blogs <joe@example.org>, \r\n\tJohn_Doe <john@example.org>");
+        assert_eq!(
+            header.get_value::<String>().unwrap(),
+            "Joe_Blogs <joe@example.org>, \r\n\tJohn_Doe <john@example.org>"
+        );
     }
 
     #[test]
@@ -405,7 +474,10 @@ mod tests {
         let addresses = vec![
             Address::new_mailbox_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
             Address::new_mailbox_with_name("John Doe".to_string(), "john@example.org".to_string()),
-            Address::new_mailbox_with_name("Mr Black".to_string(), "mafia_black@example.org".to_string()),
+            Address::new_mailbox_with_name(
+                "Mr Black".to_string(),
+                "mafia_black@example.org".to_string(),
+            ),
         ];
 
         let header = Header::new_with_value("To".to_string(), addresses).unwrap();
